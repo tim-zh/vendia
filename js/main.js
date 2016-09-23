a74b72c1d811739faf2892c899338e5a751b8cd2
tim-zh <tim.zhlobich@gmail.com>
2016-09-23 20:07:13 +0300

    2

--- js/main.js
+++ js/main.js
@@ -1,48 +1,29 @@
-/*!
- * Start Bootstrap - Freelancer Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-
 $(function() {
-	$('#cYear')[0].innerHTML = 1900 + new Date().getYear();
+  $('#cYear')[0].innerHTML = 1900 + new Date().getYear();
 
-	// jQuery for page scrolling feature - requires jQuery Easing plugin
-	$('.page-scroll a').bind('click', function(event) {
-		var $anchor = $(this);
-		$('html, body').stop().animate({
-			scrollTop: $($anchor.attr('href')).offset().top
-		}, 500);
-		event.preventDefault();
-	});
+  // jQuery for page scrolling feature - requires jQuery Easing plugin
+  $('.page-scroll a').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 500);
+    event.preventDefault();
+  });
 
+  ymaps.ready(function () {
+    var map = new ymaps.Map("map", {
+      center: [53.872201, 27.557469],
+      zoom: 17,
+      controls: ['smallMapDefaultSet'],
+      type: 'yandex#satellite'
+    });
 
-	function initialize() {
-		var mapCanvas = document.getElementById('map');
-		var center = new google.maps.LatLng(53.872201, 27.557469);
-		var mapOptions = {
-			center: center,
-			zoom: 16,
-			mapTypeId: google.maps.MapTypeId.HYBRID
-		};
-		var map = new google.maps.Map(mapCanvas, mapOptions);
-		var iw = new google.maps.InfoWindow({
-			content: "Аэродромная, 119/2"
-		});
-		var marker = new google.maps.Marker({
-			position: center,
-			map: map
-		});
-		iw.open(map, marker);
-		google.maps.event.addListener(marker, "click", function(e) {
-			iw.open(map, this);
-		});
-	}
-
-	google.maps.event.addDomListener(window, 'load', initialize);
+    myPlacemark = new ymaps.Placemark([53.872201, 27.557469], { hintContent: 'Аэродромная, 119/2' });
+    map.geoObjects.add(myPlacemark);
+  });
 });
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
-	target: '.navbar-fixed-top'
+  target: '.navbar-fixed-top'
 });
